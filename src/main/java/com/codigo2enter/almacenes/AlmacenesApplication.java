3f804ae19639a85f7ccf1ccc535b60cc23a02790
David Reyna Pineda <davidreyna@hotmail.com>
2026-05-16 05:11:24 -0600

    primer commit: estructura base de proyecto y conexion a db

---
+++ src/main/java/com/codigo2enter/almacenes/AlmacenesApplication.java
@@ -0,0 +1,13 @@
+package com.codigo2enter.almacenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo2enter/almacenes/AlmacenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo2enter.almacenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
